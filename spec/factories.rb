27e7775ae2e1d2b43764e32cebec6bef84c89477
brevous <brevous@cron.(none)>
2013-03-09 15:02:15 -0800

    working on user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Michael Armistead"
+    email    "armistead.michael@gmail.com.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
