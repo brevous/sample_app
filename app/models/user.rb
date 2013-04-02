b478d560b82a55138ca33379eb7d05db2033b54a
brevous <armistead.michael@gmail.com>
2013-04-01 21:44:46 -0700

    working on microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,14 +20,19 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:   true,
-                    format:     { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+             format:     { with: VALID_EMAIL_REGEX },
+             uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
+  def feed
+    #this is preliminary
+    Micropost.where("user_id = ?", id)
+  end
+
   private
 
-    def create_remember_token
-      self.remember_token = SecureRandom.urlsafe_base64
-    end
+  def create_remember_token
+    self.remember_token = SecureRandom.urlsafe_base64
   end
+end
